aa4483352d8f13235b6c0009d56776908250c7a8
maxisi20 <maximiliano.yanez@alumnos.ucn.cl>
2026-09-18 17:17:40 -0300

    Implementacion del cpp y hpp
    se hizo cpp y hpp para departamento (aun no se corrige el include cpp en la clase principal)

---
+++ Departamento.cpp
@@ -0,0 +1,74 @@
+#include "Departamento.hpp"
+
+Departamento::Departamento(string nombre) {
+    this->nombre = nombre;
+    this->suLista = new Lista<Paciente*>();
+}
+
+void Departamento::agregarPaciente(Paciente* paciente) {
+    suLista->añadir(paciente);
+}
+
+string Departamento::getNombre() {
+    return nombre;
+}
+
+string Departamento::toString() {
+    return this->nombre + "\n";
+}
+
+Urgencias::Urgencias() : Departamento("Urgencias") {}
+void Urgencias::mostrarEstado() {
+    cout << "--- Estado Urgencias ---" << endl;
+    cout << "Pacientes en urgencias: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+MedicinaGen::MedicinaGen() : Departamento("Medicina General") {}
+void MedicinaGen::mostrarEstado() {
+    cout << "--- Estado Medicina General ---" << endl;
+    cout << "Pacientes en Medicina General: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+Cardiologia::Cardiologia() : Departamento("Cardiologia") {}
+void Cardiologia::mostrarEstado() {
+    cout << "--- Estado Cardiologia ---" << endl;
+    cout << "Pacientes en Cardiologia: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+Neurología::Neurología() : Departamento("Neurología") {}
+void Neurología::mostrarEstado() {
+    cout << "--- Estado Neurología ---" << endl;
+    cout << "Pacientes en Neurología: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+Traumatologia::Traumatologia() : Departamento("Traumatologia") {}
+void Traumatologia::mostrarEstado() {
+    cout << "--- Estado Traumatologia ---" << endl;
+    cout << "Pacientes en Traumatologia: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+Cirugia::Cirugia() : Departamento("Cirugia") {}
+void Cirugia::mostrarEstado() {
+    cout << "--- Estado Cirugia ---" << endl;
+    cout << "Pacientes en Cirugia: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+Pediatria::Pediatria() : Departamento("Pediatria") {}
+void Pediatria::mostrarEstado() {
+    cout << "--- Estado Pediatria ---" << endl;
+    cout << "Pacientes en Pediatria: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
+
+Hospitalizacion::Hospitalizacion() : Departamento("Hospitalizacion") {}
+void Hospitalizacion::mostrarEstado() {
+    cout << "--- Estado Hospitalizacion ---" << endl;
+    cout << "Pacientes en Hospitalizacion: " << this->suLista->getSize() << endl;
+    cout << this->suLista->toString();
+}
---
+++ Departamento.hpp
@@ -0,0 +1,71 @@
+#ifndef DEPARTAMENTO_HPP
+#define DEPARTAMENTO_HPP
+
+#include <iostream>
+#include <string>
+#include "Estructuras.hpp"
+#include "Paciente.hpp"
+
+using namespace std;
+
+class Departamento {
+protected:
+    Lista<Paciente*>* suLista; 
+    string nombre;
+public:
+    Departamento(string nombre);
+    void agregarPaciente(Paciente* paciente);
+    string getNombre();
+    string toString();
+    virtual void mostrarEstado() = 0;
+};
+
+class Urgencias : public Departamento {
+public:
+    Urgencias();
+    void mostrarEstado() override;
+};
+
+class MedicinaGen : public Departamento {
+public:
+    MedicinaGen();
+    void mostrarEstado() override;
+};
+
+class Cardiologia : public Departamento {
+public:
+    Cardiologia();
+    void mostrarEstado() override;
+};
+
+class Neurología : public Departamento {
+public:
+    Neurología();
+    void mostrarEstado() override;
+};
+
+class Traumatologia : public Departamento {
+public:
+    Traumatologia();
+    void mostrarEstado() override;
+};
+
+class Cirugia : public Departamento {
+public:
+    Cirugia();
+    void mostrarEstado() override;
+};
+
+class Pediatria : public Departamento {
+public:
+    Pediatria();
+    void mostrarEstado() override;
+};
+
+class Hospitalizacion : public Departamento {
+public:
+    Hospitalizacion();
+    void mostrarEstado() override;
+};
+
+#endif
